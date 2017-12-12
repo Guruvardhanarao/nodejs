6c48bcbcd6e1606849a0dcc11a6392d0141f6d48
Guruvardhanarao <guruvardhanaraopanidam@gmail.com>
2017-12-12 11:56:36 +0530

    code added

---
+++ nodejs-examples/index.js
@@ -0,0 +1,20 @@
+var rect = {
+    area: (a,b) => (a*b),
+    perimeter: (a,b) => (2*(a+b))
+};
+
+function solveRect(l,b) {
+    console.log("Entered length=" + l + ", bredth=" + b);
+    if(l <= 0 || b <= 0) {
+        console.log("please enter length and breadth greater than zero");
+    }
+
+    else {
+        console.log("Area-(l*b)=" + rect.area(l,b));
+        console.log("Perimeter-(2(l+b)=" + rect.perimeter(l,b));
+    }
+}
+
+solveRect(2,4);
+solveRect(0,4);
+solveRect(-1,4);
