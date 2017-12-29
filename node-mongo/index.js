7f8403ed2c17d9864af00bb1d6095e3ec1209c92
Guruvardhanarao <guruvardhanaraopanidam@gmail.com>
2017-12-29 13:01:41 +0530

    Node Callback Hell and Promises

--- node-mongo/index.js
+++ node-mongo/index.js
@@ -4,32 +4,37 @@ const assert = require("assert");
 const dboprs = require('./operations');
 const url = "mongodb://localhost:27017/conFusion";
 
-MongoClient.connect(url, (err, db) =>{
-    assert.equal(err,null);
+MongoClient.connect(url).then( (db) =>{
 
     console.log("conneted correctly to server\n");
      
-    dboprs.insertDocument(db, { name: "puri", description: "I love Puri"}, "dishes", (result) =>{
+    dboprs.insertDocument(db, { name: "puri", description: "I love Puri"}, "dishes")
+       .then( (result) =>{
         console.log("Insert Document\n", result.ops);
 
-        dboprs.findDocuments(db, "dishes", (docs) =>{
+         return dboprs.findDocuments(db, "dishes");
+       })
+        .then( (docs) =>{
             console.log("Found documents", docs);
 
-            dboprs.updateDocument(db, { name: "puri"}, { description: "updated puri"}, "dishes", (result) =>{
-                console.log("updated document\n", result.result);
+            return dboprs.updateDocument(db, { name: "puri"}, { description: "updated puri"}, "dishes")
+        })
+         .then((result) =>{
+            console.log("updated document\n", result.result);
 
-                dboprs.findDocuments(db, "dishes", (docs) =>{
-                    console.log("Found updated documents", docs); 
+            return dboprs.findDocuments(db, "dishes");
+         })
+                
+          .then((docs) =>{
+                console.log("Found updated documents", docs); 
 
-                    db.dropCollection("dishes", (result) => {
-                        console.log("Dropped collection", result);
+                return db.dropCollection("dishes");
+          })
+          .then((result) => {
+                console.log("Dropped collection", result);
 
-                        db.close();
-                    });
-                });
-            });
-        });
-    });
-
-    
-});
+                db.close();
+           })
+           .catch((err) => console.log(err));
+})
+.catch((err) => console.log(err));
--- node-mongo/operations.js
+++ node-mongo/operations.js
@@ -2,42 +2,25 @@ const assert = require('assert');
 
 exports.insertDocument = (db, document, collection, callback) => {
     const coll = db.collection(collection);
-    coll.insert(document, (err, result) =>{
-        assert.equal(err, null);
-
-        console.log("Inserted " + result.result.n + " document into the collection " + collection);
-        callback(result);
-    });
+    return coll.insert(document);
 };
 
 exports.findDocuments = (db, collection, callback) => {
     const coll = db.collection(collection);
     
-    coll.find({}).toArray((err, docs) => {
-        assert.equal(err, null);
-        callback(docs);
-    });
+    return coll.find({}).toArray();
 };
 
 
 exports.updateDocument = (db, document, update, collection, callback) => {
     const coll = db.collection(collection);
 
-    coll.updateOne(document, { $set : update }, null, (err, result) => {
-        assert.equal(err, null);
-        console.log("updated the document with", update);
-        callback(result);
-    });
+    return coll.updateOne(document, { $set : update }, null);
 };
 
 
 exports.deleteDocument = (db, document, collection, callback) => {
     const coll = db.collection(collection);
 
-    coll.deleteOne(document, (err, result) =>{
-        assert.equal(err, null);
-
-        console.log("removed the document", document);
-        callback(result);
-    });
+    return coll.deleteOne(document);
 }
